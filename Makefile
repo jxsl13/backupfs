a831bf8a8e3227568a4cdd8870c8f4b26f4e9906
Behm, John <john.behm@bertelsmann.de>
2022-01-07 17:09:13 +0100

    mock fs.Stat

--- Makefile
+++ Makefile
@@ -3,6 +3,9 @@
 test:
 	go test -timeout 30s ./...
 
+coverage:
+	go test -timeout 30s -coverprofile=./coverage ./...
+	rm ./coverage
 
 fuzz:
 	gotip test -fuzz=Fuzz
--- mocks/mocks_test.go
+++ mocks/mocks_test.go
@@ -1,13 +1,51 @@
 package mocks_test
 
 import (
+	"errors"
+	"os"
 	"testing"
 
 	gomock "github.com/golang/mock/gomock"
+	"github.com/jxsl13/backupfs"
+	"github.com/jxsl13/backupfs/internal"
+	"github.com/jxsl13/backupfs/mocks"
+	"github.com/spf13/afero"
+	"github.com/stretchr/testify/assert"
 )
 
+func NewTestBackupFs(mockedBase mocks.Fs) (backupLayer, backupFs mocks.Fs) {
+	m := afero.NewMemMapFs()
+	backup := backupfs.NewPrefixFs("/backup", m)
+	return backup, backupfs.NewBackupFs(mockedBase, backup)
+}
 func TestMockFs(t *testing.T) {
+	assert := assert.New(t)
 	mockCtrl := gomock.NewController(t)
 	defer mockCtrl.Finish()
 
+	var (
+		filePath    = "/test/mock/01/test.txt"
+		expectedErr = errors.New("unknown error")
+	)
+
+	// mocked base filesystem
+	mf := mocks.NewMockFs(mockCtrl)
+	mf.EXPECT().Stat(filePath).AnyTimes().Return(nil, expectedErr)
+
+	// backupfs contains a broken basefile system
+	backup, fs := NewTestBackupFs(mf)
+
+	_, err := fs.Create(filePath)
+	assert.Error(err)
+	assert.Equal(err, expectedErr)
+
+	internal.MustNotExist(t, backup, filePath)
+
+	err = fs.Remove(filePath)
+	assert.Error(err)
+	assert.Equal(err, expectedErr)
+
+	_, err = fs.OpenFile(filePath, os.O_RDWR, 0777)
+	assert.Error(err)
+	assert.Equal(err, expectedErr)
 }
