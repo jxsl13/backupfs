cdb56472feb06934d79ef77a8a4d0d09729aab33
Behm, John <john.behm@bertelsmann.de>
2022-01-07 16:35:19 +0100

    switch filenames

--- prefixfs_test.go
+++ prefixfs_test.go
@@ -1,38 +1,30 @@
-//go:build go1.18
-// +build go1.18
-
 package backupfs
 
-import (
-	"path/filepath"
-	"regexp"
-	"testing"
+import "testing"
 
-	"github.com/stretchr/testify/assert"
-)
-
-func FuzzPrefixFs(f *testing.F) {
-
-	expectedPrefix, err := filepath.Abs("./tests/prefix")
-	if err != nil {
-		f.Fatal(err)
+func Test_removeStrings(t *testing.T) {
+	type args struct {
+		lookupIn string
+		applyTo  string
+		remove   []string
 	}
-
-	const fileName = "prefixfs_test.txt"
-	fs := NewTestPrefixFs(expectedPrefix)
-	for _, seed := range []string{".", "/", "..", "\\", fileName} {
-		f.Add(seed)
+	tests := []struct {
+		name        string
+		args        args
+		wantCleaned string
+		wantFound   bool
+	}{
+		{"#1", args{"..prefixfs_test0txt/../..", "..prefixfs_test0txt/../..", []string{"../", "./", ".."}}, "prefixfs_test0txt/", true},
 	}
-
-	filenameRegex := regexp.MustCompile(`[^\d]` + fileName)
-
-	f.Fuzz(func(t *testing.T, input string) {
-		if !filenameRegex.MatchString(input) {
-			return
-		}
-
-		assert := assert.New(t)
-		outputPath := fs.prefixPath(input)
-		assert.Contains(outputPath, expectedPrefix)
-	})
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			gotCleaned, gotFound := removeStrings(tt.args.lookupIn, tt.args.applyTo, tt.args.remove...)
+			if gotCleaned != tt.wantCleaned {
+				t.Errorf("removeStrings() gotCleaned = '%v', want '%v'", gotCleaned, tt.wantCleaned)
+			}
+			if gotFound != tt.wantFound {
+				t.Errorf("removeStrings() gotFound = %v, want %v", gotFound, tt.wantFound)
+			}
+		})
+	}
 }
