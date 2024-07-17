ce98defdf0b82f3615cebe5b06afa797df2f1afd
John Behm <jxsl13@googlemail.com>
2024-07-17 13:17:40 +0200

    trim volume on windows for sorting

--- sort.go
+++ sort.go
@@ -15,8 +15,11 @@ type ByMostFilePathSeparators []string
 func (a ByMostFilePathSeparators) Len() int      { return len(a) }
 func (a ByMostFilePathSeparators) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ByMostFilePathSeparators) Less(i, j int) bool {
-	ca := strings.Count(a[i], separator)
-	cb := strings.Count(a[j], separator)
+	ai := TrimVolume(a[i])
+	aj := TrimVolume(a[j])
+
+	ca := strings.Count(ai, separator)
+	cb := strings.Count(aj, separator)
 
 	/*
 		Edge case where the root path is compared to a file in the root path.
@@ -27,17 +30,19 @@ func (a ByMostFilePathSeparators) Less(i, j int) bool {
 	*/
 
 	// root = smallest number of separators
-	if ca == 1 && a[i] == separator {
+	if ca == 1 && ai == separator {
 		ca = -1
 	}
 
-	if cb == 1 && a[j] == separator {
+	if cb == 1 && aj == separator {
 		cb = -1
 	}
 
 	if ca == cb {
+		// with volume
 		return a[i] < a[j]
 	}
+
 	return ca > cb
 }
 
@@ -49,8 +54,11 @@ type ByLeastFilePathSeparators []string
 func (a ByLeastFilePathSeparators) Len() int      { return len(a) }
 func (a ByLeastFilePathSeparators) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ByLeastFilePathSeparators) Less(i, j int) bool {
-	ca := strings.Count(a[i], separator)
-	cb := strings.Count(a[j], separator)
+	ai := TrimVolume(a[i])
+	aj := TrimVolume(a[j])
+
+	ca := strings.Count(ai, separator)
+	cb := strings.Count(aj, separator)
 
 	/*
 		Edge case where the root path is compared to a file in the root path.
@@ -61,15 +69,16 @@ func (a ByLeastFilePathSeparators) Less(i, j int) bool {
 	*/
 
 	// root = smallest number of separators
-	if ca == 1 && a[i] == separator {
+	if ca == 1 && ai == separator {
 		ca = -1
 	}
 
-	if cb == 1 && a[j] == separator {
+	if cb == 1 && aj == separator {
 		cb = -1
 	}
 
 	if ca == cb {
+		// with volume
 		return a[i] < a[j]
 	}
 	return ca < cb
