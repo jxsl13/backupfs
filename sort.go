d0fd7ae70b9832c02eba726016f2b2817a8f6d26
John Behm <jxsl13@googlemail.com>
2024-08-08 09:29:55 +0200

    introduce public less function for filepath separators (can be used for sorting of other ovjects that have file paths as fields

--- sort.go
+++ sort.go
@@ -15,35 +15,7 @@ type ByMostFilePathSeparators []string
 func (a ByMostFilePathSeparators) Len() int      { return len(a) }
 func (a ByMostFilePathSeparators) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ByMostFilePathSeparators) Less(i, j int) bool {
-	ai := TrimVolume(a[i])
-	aj := TrimVolume(a[j])
-
-	ca := strings.Count(ai, separator)
-	cb := strings.Count(aj, separator)
-
-	/*
-		Edge case where the root path is compared to a file in the root path.
-		[0] = "/test/0/2"
-		[1] = "/test/0"
-		[2] = "/"
-		[3] = "/test"
-	*/
-
-	// root = smallest number of separators
-	if ca == 1 && ai == separator {
-		ca = -1
-	}
-
-	if cb == 1 && aj == separator {
-		cb = -1
-	}
-
-	if ca == cb {
-		// with volume
-		return a[i] < a[j]
-	}
-
-	return ca > cb
+	return !LessFilePathSeparators(a[i], a[j])
 }
 
 // ByLeastFilePathSeparators sorts the string by the number of file path separators
@@ -54,8 +26,14 @@ type ByLeastFilePathSeparators []string
 func (a ByLeastFilePathSeparators) Len() int      { return len(a) }
 func (a ByLeastFilePathSeparators) Swap(i, j int) { a[i], a[j] = a[j], a[i] }
 func (a ByLeastFilePathSeparators) Less(i, j int) bool {
-	ai := TrimVolume(a[i])
-	aj := TrimVolume(a[j])
+	return LessFilePathSeparators(a[i], a[j])
+}
+
+// LessFilePathSeparators compares two file paths by the number of file path separators
+// returns true if a has less file path separators than b
+func LessFilePathSeparators(a, b string) bool {
+	ai := TrimVolume(a)
+	aj := TrimVolume(a)
 
 	ca := strings.Count(ai, separator)
 	cb := strings.Count(aj, separator)
@@ -79,7 +57,7 @@ func (a ByLeastFilePathSeparators) Less(i, j int) bool {
 
 	if ca == cb {
 		// with volume
-		return a[i] < a[j]
+		return a < b
 	}
 	return ca < cb
 }
