51299c26793421ac50b0537cd9979ba3adf5fba2
John Behm <jxsl13@googlemail.com>
2024-08-09 16:37:14 +0200

    remove windows specific options, always allow windows volumes

--- backupfs_options.go
+++ backupfs_options.go
@@ -1,13 +1,3 @@
 package backupfs
 
-type backupFSOptions struct {
-	allowWindowsVolumePaths bool
-}
-
-// WithVolumePaths, contrary to the normal BackupFS, this variant allows
-// to use absolute windows paths (C:\A\B\C instead of \A\B\C) when set to true.
-func WithVolumePaths(allow bool) BackupFSOption {
-	return func(bf *backupFSOptions) {
-		bf.allowWindowsVolumePaths = allow
-	}
-}
+type backupFSOptions struct{}
